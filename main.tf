567fc86c9d9bbb17726439bc786fdd7b6f0d80aa
Kozma Hunor <82783681+kozmahubor@users.noreply.github.com>
2024-03-31 22:40:29 +0200

    .

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 
-provider "registry.terraform.io/hashicorp/google" {
+provider "google" {
   credentials = file(var.gcp_credentials_file)
   project     = var.project_id
   region      = var.region
