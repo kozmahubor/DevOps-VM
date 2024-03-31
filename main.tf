80d26f7cbc91c4789cc9527e54f17bdd339790f2
Kozma Hunor <82783681+kozmahubor@users.noreply.github.com>
2024-03-31 21:27:20 +0200

    test

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.5"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.gcp_credentials_file)
+  project     = var.project_id
+  region      = var.region
+}
+
+resource "google_compute_instance" "default" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-12-bookworm-v20240312"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "gcp_credentials_file" {
+  description = "The GCP credentials file"
+  type        = string
+}
+
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+}
